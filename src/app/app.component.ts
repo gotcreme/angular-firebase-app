46be8ab7500f7b158416277798dd3862b0df6dea
gotcreme <193creme@gmail.com>
2017-10-29 22:44:15 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database'
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  courses$;
+  courseList: AngularFireList<{}>;
+  course$;
+  author$;
+
+  constructor(private db: AngularFireDatabase) {
+    this.courses$ = db.list('/courses').snapshotChanges();
+    this.courseList = db.list('/courses');
+    this.author$ = db.object('/authors/1').valueChanges();
+  }
+
+  add(course: HTMLInputElement) {
+    this.courseList.push({
+      name: course.value,
+      price: 150,
+      isLive: true,
+      sections: [
+        { title: 'Components' },
+        { title: 'Directives' },
+        { title: 'Templates' },
+      ]
+    });
+
+    course.value = '';
+  }
+
+  update(course) {
+    this.db.object('/courses/' + course.key)
+      .set(course.payload.val() + ' UPDATED');
+    
+    this.db.object('/courses/' + course.key)
+      .set({
+        title: 'course' + course.key + ' UPDATED',
+        price: 150
+      });
+  }
+
+  delete(course) {
+    this.db.object('/courses/' + course.key)
+      .remove()
+      .then(() => {
+        console.log("deleted");
+      });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCUkTs3rZ1EH83jgJ0MRS2VDYFb4YJU3CQ",
+    authDomain: "fir-demo-4b72f.firebaseapp.com",
+    databaseURL: "https://fir-demo-4b72f.firebaseio.com",
+    projectId: "fir-demo-4b72f",
+    storageBucket: "fir-demo-4b72f.appspot.com",
+    messagingSenderId: "528486263"
+  }
 };
